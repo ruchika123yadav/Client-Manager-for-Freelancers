8a28001fa2443738d305fb0075f901eb1c0e984e
sachi_2162 <sharmasachidanand1111@gmail.com>
2025-05-03 21:14:58 +0530

    updated clientform.

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -5,12 +5,20 @@ module.exports.clientForm=async(req,res)=>{
     res.render('clientform')
 }
 
-module.exports.addClientInfo=async(req,res)=>{
-    let newClient =req.body
-    const client= new Client(newClient)
-    await client.save()
-    res.redirect("/clients/addproject")
-}
+module.exports.addClientInfo = async (req, res) => {
+    try {
+      let { name, email, notes, company, phone } = req.body;
+    
+  
+      await Client.create({ name, email, notes, company, phone });
+  
+      res.redirect("/client/addproject");
+    } catch (err) {
+      console.error("Error adding client:", err);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+  
 
 module.exports.addProject=async(req,res)=>{
     res.render('addProject')
