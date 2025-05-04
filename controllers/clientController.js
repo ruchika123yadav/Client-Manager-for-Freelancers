0d6d07a313ec92113964ad554467e4ff55a6a73e
ruchika123yadav <145842561+ruchika123yadav@users.noreply.github.com>
2025-05-04 20:47:06 +0530

    update

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -2,15 +2,27 @@ const Client =require("../models/client.js")
 
 
 module.exports.clientForm=async(req,res)=>{
-    res.render('clientform')
+    const clientId=req.params.id
+    const client=await Client.findById(clientId).populate('userId')
+    res.render('clientform',{ clientId })
 }
 
-module.exports.addClientInfo=async(req,res)=>{
-    let newClient =req.body
-    const client= new Client(newClient)
-    await client.save()
-    res.redirect("/clients/addproject")
-}
+module.exports.addClientInfo = async (req, res) => {
+    try {
+        // const { name, email, notes, company, phone } = req.body;
+           console.log(req.body)
+        // await Client.create({ name, email, notes, company, phone });
+        const newClient = new Client(req.body.client)
+         newClient.userId=req.user._id
+          await newClient.save();
+        console.log("Client added successfully:", newClient);
+        res.redirect("/clients/addproject");
+    } catch (err) {
+        console.error("Error adding client:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
 
 module.exports.addProject=async(req,res)=>{
     res.render('addProject')
