5e345d1922aab09c3457ae455c8a4953dc8defe3
ruchika123yadav <145842561+ruchika123yadav@users.noreply.github.com>
2025-05-04 20:57:01 +0530

    update

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -2,29 +2,13 @@ const Client =require("../models/client.js")
 
 
 module.exports.clientForm=async(req,res)=>{
-    const clientId=req.params.id
-    const client=await Client.findById(clientId).populate('userId')
-    res.render('clientform',{ clientId })
+    // const clientId=req.params.id
+    // const client=await Client.findById(clientId).populate('userId')
+    res.render('clientform')
 }
 
 module.exports.addClientInfo = async (req, res) => {
     try {
-<<<<<<< HEAD
-        // const { name, email, notes, company, phone } = req.body;
-           console.log(req.body)
-        // await Client.create({ name, email, notes, company, phone });
-        const newClient = new Client(req.body.client)
-         newClient.userId=req.user._id
-          await newClient.save();
-        console.log("Client added successfully:", newClient);
-        res.redirect("/clients/addproject");
-    } catch (err) {
-        console.error("Error adding client:", err);
-        res.status(500).json({ error: "Internal server error" });
-    }
-};
-
-=======
       let { name, email, notes, company, phone } = req.body;
     
   
@@ -37,7 +21,6 @@ module.exports.addClientInfo = async (req, res) => {
     }
   };
   
->>>>>>> 414c31a6a030eb1b0b9034f70c8621a1186a48ea
 
 module.exports.addProject=async(req,res)=>{
     res.render('addProject')
--- routers/client.js
+++ routers/client.js
@@ -5,9 +5,9 @@ const isLoggedIn=require("../middlewares/isLoggedIn.js")
 
 const Client=require("../models/client.js")
 
-router.get("/:id/info",isLoggedIn,clientController.clientForm)
+router.get("/info",isLoggedIn,clientController.clientForm)
 
-router.post("/:id/info",isLoggedIn,clientController.addClientInfo)
+router.post("/info",isLoggedIn,clientController.addClientInfo)
 
 router.get("/addproject",isLoggedIn,clientController.addProject)
 
